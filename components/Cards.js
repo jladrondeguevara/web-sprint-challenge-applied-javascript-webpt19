0b35a7241d941f6ea90828c309d10ca7e96836dd
Jeovanni <jeovannildg@gmail.com>
2020-07-19 21:38:07 -0600

    added card elements

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,20 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then((response) => {
+    console.log(response.data.articles);
+})
+
+function Card() {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgDiv = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');  
+
+    return card;
+}
